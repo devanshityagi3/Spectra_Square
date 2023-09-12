effb988ec8c7cdd2281494a3a50879e987c848ec
Devanshi <devanshityagi3@gmail.com>
2023-09-12 20:41:12 +0530

    search bar added

--- UI/src/components/ProductList.js
+++ UI/src/components/ProductList.js
@@ -1,20 +1,9 @@
-import { FaSpinner } from "react-icons/fa";
 import Navbar from "./Navbar";
-import ProdItem from "./ProdItem";
-import useFetch from "./useFetch";
 import Filters from "./Filters";
 import Search from "./Search";
 
 
 const ProductListPage = () => {
-  
-  const { data, isPending, error } = useFetch("http://localhost:8000/products");
-  if (error) {
-    return <div>Error: {error}</div>;
-  } else {
-    console.log("data loaded", data, isPending, error);
-  }
-
   return (
     <div>
       <Navbar></Navbar>
@@ -23,26 +12,6 @@ const ProductListPage = () => {
       </div>
       <div className="flex flex-row gap-3 m-5">
         <Filters />
-        <div className="flex flex-row">
-          {isPending ? (
-            <div>
-              <FaSpinner />
-            </div>
-          ) : data?.length !== 0 ? (
-            data.map((prod) => (
-              <ProdItem
-                key={prod.id}
-                pname={prod.pname}
-                pimage={prod.pimage}
-                pprice={prod.pprice}
-              ></ProdItem>
-            ))
-          ) : (
-            <div>
-              <FaSpinner />
-            </div>
-          )}
-        </div>
       </div>
     </div>
   );
